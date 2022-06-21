a5369cea26cd618cc1cc12d22694beb735446fd1
pressMachine <nnenadkkrstic@gmail.com>
2022-06-21 19:46:01 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from "axios";
+
 
 function App() {
+  axios.defaults.baseURL = "http://localhost:5000";
+  const [name, setName] = useState("strer");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Submit</button>
+      <input type = "text"></input>
     </div>
   );
 }
